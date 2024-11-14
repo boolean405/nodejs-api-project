06e60d269e83269f3bbdd224e6a6718f6fe561ef
Boolean <boolean05@gmail.com>
2024-11-15 02:30:32 +0700

    fix redis

--- server/utils/redis.js
+++ server/utils/redis.js
@@ -1,25 +1,25 @@
 // localhost Redis
-// const Redis = require("async-redis").createClient();
+const Redis = require("async-redis").createClient()
 
 require("dotenv").config();
 
 // Server Redis
-const Redis = require("redis")
-  .createClient({
-    password: process.env.REDIS_PASSWORD,
-    socket: {
-      host: process.env.REDIS_HOST,
-      port: process.env.REDIS_PORT,
-    },
-  })
+// const Redis = require("redis")
+//   .createClient({
+//     password: process.env.REDIS_PASSWORD,
+//     socket: {
+//       host: process.env.REDIS_HOST,
+//       port: process.env.REDIS_PORT,
+//     },
+//   })
   // .on("error", (err) => console.log("Redis Client Error", err))
-  .connect()
-  .then(() => {
-    console.log("Success, Connected to Redis");
-  })
-  .catch((err) => {
-    console.log("Failed to connect to Redis:", err);
-  });
+  // .connect()
+  // .then(() => {
+  //   console.log("Success, Connected to Redis");
+  // })
+  // .catch((err) => {
+  //   console.log("Failed to connect to Redis:", err);
+  // });
 
 
 module.exports = {
